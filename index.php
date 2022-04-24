fc3200fcd764890a5a118d0c4c1ae46057b7b520
Sv3njaM <svenja-meyer@arcor.de>
2022-04-24 11:26:53 +0200

    start create folder/navigation/header ...

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+//index.php is navigating to the different folders
+session_start();
+error_reporting(-1);
+ini_set('display_errors', 'On');
+
+//creating constant variables for the directories
+define('CONFIG_DIR', __DIR__.'/config');
+define('ASSETS_DIR', __DIR__.'/assets');
+define('TEMPLATES_DIR', __DIR__.'/templates');
+define('LOG_DIR', __DIR__.'/logs');
+define('STORAGE_DIR', __DIR__.'/storage');
+define('BIN_DIR', __DIR__.'/bin');
+
+//includes.php have all needed pages inside for only one include
+require __DIR__.'/includes.php';
---
+++ templates/navbar.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container">
+    <a class="navbar-brand" href="#">Onlineshop</a>
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="index.php/logout">Logout</a>
+        <a class="nav-link" href="index.php/login">Login</a>
+      </li>
+        <li class="nav-item">
+          <a class="nav-link" href="index.php/register">Registrate</a>
+        </li>
+    </ul>
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a href="index.php/cart">Warenkorb</a>
+      </li>
+    </ul>
+  </div>
+</nav>
