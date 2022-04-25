0f7fe611d19d30729313dfb28bc84c80157bdb13
Sv3njaM <svenja-meyer@arcor.de>
2022-04-25 20:03:23 +0200

    admin access for create and edit products

--- templates/main.php
+++ templates/main.php
@@ -1,6 +1,8 @@
 <?php require_once __DIR__.'/header.php' ?>
 <section class="container">
   <div class="row">
+    <!--check if Admin rights are available to create a new product-->
+    <?php if($isAdmin): ?>
     <div class="col">
       <!--part with a plus to add a new product into the shop database-->
       <div class="card" id="newProduct">
@@ -11,6 +13,7 @@
         </div>
       </div>
     </div>
+    <?php endif;?>
     <!-- In this part the card.php is included and the products shown -->
     <?php foreach($products as $product):?>
     <div class="col-3">
