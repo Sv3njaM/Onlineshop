77ba6a8e7d29fe7037d2146ac8f31849c741ba64
Sv3njaM <svenja-meyer@arcor.de>
2022-05-05 16:15:00 +0200

    thank you page

---
+++ templates/thankYouPage.php
@@ -0,0 +1,8 @@
+<?php require_once __DIR__.'/header.php' ?>
+
+<section class="container">
+  Your order have been confirmed.
+  Thank you very much!
+</section>
+
+<?php require_once __DIR__.'/header.php' ?>
